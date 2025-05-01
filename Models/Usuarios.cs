535cf8c38597ec56a4056ebb57072eb779779b0d
h0ndv <118297178+h0ndv@users.noreply.github.com>
2025-05-01 19:44:04 -0400

    feat: Añadir CRUD de Usuarios

---
+++ Models/Usuarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectocsharp.Models
+{
+    internal class Usuarios
+    {
+        private string usuario;
+        private string contraseña;
+
+
+        public Usuarios(string usuario, string contraseña)
+        {
+            this.usuario = usuario;
+            this.contraseña = contraseña;
+        }
+
+        // Getters y Setters
+        public string GetUsuario()
+        {
+            return usuario;
+        }
+
+        public void SetUsuario(string usuario)
+        {
+            this.usuario = usuario;
+        }
+
+        public string GetContraseña()
+        {
+            return contraseña;
+        }
+
+        public void SetContraseña(string contraseña)
+        {
+            this.contraseña = contraseña;
+        }
+    }
+}
---
+++ Services/GestorUsuarios.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using proyectocsharp.Models;
+
+namespace proyectocsharp
+{
+    internal class GestorUsuarios
+    {
+        // Instanciar la clase Conexion
+        Conexion conexion = new Conexion();
+
+        public string validarUsuario(Usuarios usuarios)
+        {
+            try
+            {
+                // Consulta sql
+                string consulta = "SELECT * FROM Usuarios WHERE Usuario = @usuario AND Password = @contraseña";
+
+                // Abrir la conexion
+                SqlCommand sqlCommand = new SqlCommand(consulta, conexion.AbrirConexion());
+
+                // Parametros de la consulta
+                sqlCommand.Parameters.AddWithValue("@usuario", usuarios.GetUsuario());
+                sqlCommand.Parameters.AddWithValue("@contraseña", usuarios.GetContraseña());
+
+                // Ejecutar la consulta
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                // Verificar si el usuario existe
+                return sqlDataReader.HasRows ? null : "Usuario o contraseña incorrectos";
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+                Console.WriteLine("Error: " + ex.Message);
+                return $"Error al validar el usuario: {ex.Message}";
+            }
+            finally
+            {
+                // Cerrar la conexion
+                conexion.CerrarConexion();
+            }
+        }
+
+        public void AgregarUsuario(Usuarios usuarios)
+        {
+            try
+            {
+                // Consulta sql
+                string consulta = "INSERT INTO Usuarios (Usuario, Password) VALUES (@usuario, @contraseña)";
+
+                // Abrir la conexion
+                SqlCommand sqlCommand = new SqlCommand(consulta, conexion.AbrirConexion());
+
+                // Parametros de la consulta
+                sqlCommand.Parameters.AddWithValue("@usuario", usuarios.GetUsuario());
+                sqlCommand.Parameters.AddWithValue("@contraseña", usuarios.GetContraseña());
+
+                // Ejecutar la consulta
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                // Cerrar la conexion
+                conexion.CerrarConexion();
+            }
+        }
+
+        public void EliminarUsuario(Usuarios usuarios)
+        {
+            try
+            {
+                // Consulta sql
+                string consulta = "DELETE FROM Usuarios WHERE Usuario = @usuario";
+
+                // Abrir la conexion
+                SqlCommand sqlCommand = new SqlCommand(consulta, conexion.AbrirConexion());
+
+                // Parametros de la consulta
+                sqlCommand.Parameters.AddWithValue("@usuario", usuarios.GetUsuario());
+
+                // Ejecutar la consulta
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                // Cerrar la conexion
+                conexion.CerrarConexion();
+            }
+        }
+
+        public void ModificarUsuario(Usuarios usuarios)
+        {
+            try
+            {
+                // Consulta sql
+                string consulta = "UPDATE Usuarios SET Password = @contraseña WHERE Usuario = @usuario";
+
+                // Abrir la conexion
+                SqlCommand sqlCommand = new SqlCommand(consulta, conexion.AbrirConexion());
+
+                // Parametros de la consulta
+                sqlCommand.Parameters.AddWithValue("@usuario", usuarios.GetUsuario());
+                sqlCommand.Parameters.AddWithValue("@contraseña", usuarios.GetContraseña());
+                
+                // Ejecutar la consulta
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                // Cerrar la conexion
+                conexion.CerrarConexion();
+            }
+        }
+    }
+}
